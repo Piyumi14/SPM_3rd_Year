dd51a324b7ee9d9706ab999dab5ae82bd7994bff
duranka-kads <durankaranaweera@gmail.com>
2020-08-20 03:10:31 +0530

    create subject statics model

---
+++ src/spm/AsubjectStacModel.java
@@ -0,0 +1,130 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spm;
+
+/**
+ *
+ * @author User
+ */
+public class AsubjectStacModel {
+    
+      private String scode;
+    private String year;
+    private String sem;
+   
+     private String noOfLecHr;
+    private String noOfTuteHr;
+    private String noOfLabHr;
+      private String noOfEvlHr;
+
+    /**
+     * @return the scode
+     */
+    public String getScode() {
+        return scode;
+    }
+
+    /**
+     * @param scode the scode to set
+     */
+    public void setScode(String scode) {
+        this.scode = scode;
+    }
+
+    /**
+     * @return the year
+     */
+    public String getYear() {
+        return year;
+    }
+
+    /**
+     * @param year the year to set
+     */
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    /**
+     * @return the sem
+     */
+    public String getSem() {
+        return sem;
+    }
+
+    /**
+     * @param sem the sem to set
+     */
+    public void setSem(String sem) {
+        this.sem = sem;
+    }
+
+    /**
+     * @return the dept
+  
+
+    /**
+     * @return the NoOFSub
+     */
+   
+
+    /**
+     * @return the noOfLecHr
+     */
+    public String getNoOfLecHr() {
+        return noOfLecHr;
+    }
+
+    /**
+     * @param noOfLecHr the noOfLecHr to set
+     */
+    public void setNoOfLecHr(String noOfLecHr) {
+        this.noOfLecHr = noOfLecHr;
+    }
+
+    /**
+     * @return the noOfTuteHr
+     */
+    public String getNoOfTuteHr() {
+        return noOfTuteHr;
+    }
+
+    /**
+     * @param noOfTuteHr the noOfTuteHr to set
+     */
+    public void setNoOfTuteHr(String noOfTuteHr) {
+        this.noOfTuteHr = noOfTuteHr;
+    }
+
+    /**
+     * @return the noOfLabHr
+     */
+    public String getNoOfLabHr() {
+        return noOfLabHr;
+    }
+
+    /**
+     * @param noOfLabHr the noOfLabHr to set
+     */
+    public void setNoOfLabHr(String noOfLabHr) {
+        this.noOfLabHr = noOfLabHr;
+    }
+
+    /**
+     * @return the noOfEvlHr
+     */
+    public String getNoOfEvlHr() {
+        return noOfEvlHr;
+    }
+
+    /**
+     * @param noOfEvlHr the noOfEvlHr to set
+     */
+    public void setNoOfEvlHr(String noOfEvlHr) {
+        this.noOfEvlHr = noOfEvlHr;
+    }
+    
+}
