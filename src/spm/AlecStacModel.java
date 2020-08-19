e8cd41e875d60bec93279d9addc58296ed1e6173
duranka-kads <durankaranaweera@gmail.com>
2020-08-20 03:15:12 +0530

    create lecture static model

---
+++ src/spm/AlecStacModel.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spm;
+
+/**
+ *
+ * @author User
+ */
+public class AlecStacModel {
+    
+     
+    private String category;
+    private String scenter;
+    private String faculty;
+    private String dept;
+    private String NoOFSub;
+
+    /**
+     * @return the category
+     */
+    public String getCategory() {
+        return category;
+    }
+
+    /**
+     * @param category the category to set
+     */
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    /**
+     * @return the scenter
+     */
+    public String getScenter() {
+        return scenter;
+    }
+
+    /**
+     * @param scenter the scenter to set
+     */
+    public void setScenter(String scenter) {
+        this.scenter = scenter;
+    }
+
+    /**
+     * @return the faculty
+     */
+    public String getFaculty() {
+        return faculty;
+    }
+
+    /**
+     * @param faculty the faculty to set
+     */
+    public void setFaculty(String faculty) {
+        this.faculty = faculty;
+    }
+
+    /**
+     * @return the dept
+     */
+    public String getDept() {
+        return dept;
+    }
+
+    /**
+     * @param dept the dept to set
+     */
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+
+    /**
+     * @return the NoOFSub
+     */
+    public String getNoOFSub() {
+        return NoOFSub;
+    }
+
+    /**
+     * @param NoOFSub the NoOFSub to set
+     */
+    public void setNoOFSub(String NoOFSub) {
+        this.NoOFSub = NoOFSub;
+    }
+  
+
+    
+}
